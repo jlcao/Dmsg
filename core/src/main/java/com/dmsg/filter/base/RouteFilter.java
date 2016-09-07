0cb863b4158e17f66b748a82832d4dd4fe1e29e9
jlcao <864497178@qq.com>
2016-09-07 12:34:23 +0800

    目录变更

---
+++ core/src/main/java/com/dmsg/filter/base/RouteFilter.java
@@ -0,0 +1,35 @@
+package com.dmsg.filter.base;
+
+import com.dmsg.filter.DmsgFilter;
+import com.dmsg.filter.FilterChain;
+import com.dmsg.message.MessageContext;
+import com.dmsg.route.RouteHandler;
+import com.dmsg.server.DmsgServerContext;
+
+/**
+ * Created by jlcao on 2016/8/14.
+ */
+public class RouteFilter extends DmsgFilter {
+    private RouteHandler routeHandler;
+
+    public RouteFilter() {
+        routeHandler = DmsgServerContext.getServerContext().getRouteHandler();
+    }
+
+    public void doFilter(MessageContext messageContext, FilterChain chain) {
+        try {
+            routeHandler.route(messageContext);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        chain.doFilter(messageContext);
+    }
+
+    public void destroy() {
+
+    }
+
+    public void init() {
+
+    }
+}
